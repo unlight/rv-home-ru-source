e5871fbc9d83c3c3b5e50a0eb5768aaa2e02bcc1
iamthes <s91630277@gmail.com>
2011-12-03 15:51:42 +0400

    Removed nofollow attribute

--- views/post.php
+++ views/post.php
@@ -7,7 +7,7 @@ if (!isset($AllowEdit)) $AllowEdit = GetValue('PHP_AUTH_PW', $_SERVER);
 <h3><?php echo $Post->Title;?></h3>
 <?php echo $Post->Body;?>
 <p class="date"><?php
-	if ($Post->Link) echo '<a rel="nofollow" href="', $Post->Link, '">Читать полностью</a> | ';
+	if ($Post->Link) echo '<a href="', $Post->Link, '">Читать полностью</a> | ';
 	echo date('D F j, Y', $PostTimeStamp); // Sat April 29, 2006
 	if ($AllowEdit) echo " | <a href='?id={$Post->PostID}&action=post'>редактировать</a>";
 ?></p>
