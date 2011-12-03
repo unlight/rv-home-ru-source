7d255340343062e58017c77307d773059241e1e1
iamthes <s91630277@gmail.com>
2011-12-03 19:21:09 +0400

    Remote link

--- views/post.php
+++ views/post.php
@@ -2,12 +2,19 @@
 $Post = (object)$Post;
 $PostTimeStamp = strtotime($Post->PostDate);
 if (!isset($AllowEdit)) $AllowEdit = GetValue('PHP_AUTH_PW', $_SERVER);
+$RemoteHost = '';
+if ($Post->Link) {
+	$RemoteHost = parse_url($Post->Link, PHP_URL_HOST);
+	if (substr($RemoteHost, 0, 4) == 'www.') $RemoteHost = substr($RemoteHost, 4);
+}
+
 ?>
 
 <h3><?php echo $Post->Title;?></h3>
 <?php echo $Post->Body;?>
 <p class="date"><?php
-	if ($Post->Link) echo '<a href="', $Post->Link, '">Читать полностью</a> | ';
+	if ($RemoteHost) echo "<a href=\"{$Post->Link}\">Полностью на $RemoteHost</a> | ";
+	if ($Post->HasFullBody) echo "<a href=\"?p={$Post->PostID}\">Прочитать полностью (здесь)</a> | ";
 	echo date('D F j, Y', $PostTimeStamp); // Sat April 29, 2006
 	if ($AllowEdit) echo " | <a href='?id={$Post->PostID}&action=post'>редактировать</a>";
 ?></p>
