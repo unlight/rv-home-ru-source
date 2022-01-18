ce615c8117eb526f6720c2a2b6eeccd991b8efda
Roman_Vasilev <s91630277@gmail.com>
2022-01-18 15:07:52 +0400

    uncommented vk

--- views/index.php
+++ views/index.php
@@ -5,11 +5,11 @@
 	<p><a class="GitHub" rel="nofollow" href="https://github.com/unlight">github.com/unlight</a></p>
 	<!-- <p><a class="Npm" rel="nofollow" href="https://npmjs.org/~iamthes">npmjs.org/~iamthes</a></p> -->
 	<p><a class="WebHive" rel="nofollow" href="http://webhive.herokuapp.com">webhive.info</a></p>
+	<p><a class="Vk" rel="nofollow" href="https://vk.com/id144809117">vk.com/id144809117</a></p>
 	<p><a class="Vanilla" rel="nofollow" href="http://vanillaforums.org/profile/s">vanillaforums.org/profile/8576/s</a></p>
-	<p><a class="AskDev Dead" rel="nofollow" href="http://www.askdev.ru/member/iamthes/show/answered/">askdev.ru/member/iamthes</a></p>
-	<p><a class="UT2004 Dead" rel="nofollow" href="http://sst.planetunreal.ru/">sst.planetunreal.ru</a></p>
-	<!-- <p><a class="Vk" rel="nofollow" href="https://vk.com/id144809117">vk.com/id144809117</a></p> -->
 	<p><a class="Garden Dead" href="http://gardenplatform.wordpress.com/">gardenframework.ru</a></p>
+	<p><a class="UT2004 Dead" rel="nofollow" href="http://sst.planetunreal.ru/">sst.planetunreal.ru</a></p>
+	<!-- <p><a class="AskDev Dead" rel="nofollow" href="http://www.askdev.ru/member/iamthes/show/answered/">askdev.ru/member/iamthes</a></p> -->
 	<p><a class="LetsGoToTheCinema Dead" href="http://web.archive.org/web/http://lets-go-to-the-cinema.ru/">lets-go-to-the-cinema.ru</a></p>
 	<p><a class="Toogle Dead" href="http://web.archive.org/web/20100306055838/http://toogle.vfose.ru/">toogle.vfose.ru</a></p>
 </div>
