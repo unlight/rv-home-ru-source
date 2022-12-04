baaf90c9b986de9c1a613b478814fd38e5787689
Roman_Vasilev <s91630277@gmail.com>
2022-12-05 01:53:18 +0400

    Removed posts and contacts

--- views/index.php
+++ views/index.php
@@ -10,34 +10,6 @@
 	<p><a class="Vanilla" rel="nofollow" href="http://vanillaforums.org/profile/s">vanillaforums.org/profile/8576/s</a></p>
 	<p><a class="Garden Dead" href="http://gardenplatform.wordpress.com/">gardenframework.ru</a></p>
 	<p><a class="UT2004 Dead" rel="nofollow" href="http://sst.planetunreal.ru/">sst.planetunreal.ru</a></p>
-	<!-- <p><a class="AskDev Dead" rel="nofollow" href="http://www.askdev.ru/member/iamthes/show/answered/">askdev.ru/member/iamthes</a></p> -->
 	<p><a class="LetsGoToTheCinema Dead" href="http://web.archive.org/web/http://lets-go-to-the-cinema.ru/">lets-go-to-the-cinema.ru</a></p>
 	<p><a class="Toogle Dead" href="http://web.archive.org/web/20100306055838/http://toogle.vfose.ru/">toogle.vfose.ru</a></p>
-</div>
-
-<!--<a name="preamble"></a>
-<div id="subheader">
- <h2>Интро</h2>
-	<p>Добрый день! 
-	Здесь должен быть раздел обо мне, но его пока нет.
-	<a href="blog">Посты</a> это коллекция ссылок на мои статьи, сообщения и комментарии на разные темы в интернете.
-	Если хотите связаться со мной, смотрите раздел <a href="#contact">контакты</a>.</p>
-</div> -->
-
-<?php if (isset($Posts) && count($Posts) > 0) { ?>
-	<div id="blog">
-		<h2>Заметки</h2>
-		<?php foreach ($Posts as $Post) {
-			include dirname(__FILE__).'/post.php';
-		}
-		?>
-	</div>
-<?php } ?>
-
-<div id="contact">
-	<h2>Контакты</h2>
-	<p>
-	Email: s91630277 [at] gmail [dot] com<br/>
-	<!-- Skype: k91630277<br/> -->
-	</p>
 </div>
